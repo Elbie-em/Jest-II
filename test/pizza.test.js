9aa0e68e3efd5c3fd2825defcf1cf90a1e42ebf7
Elbie-em <emoonga1993@gmail.com>
2020-08-24 19:06:07 +0200

    Add test for length

--- test/pizza.test.js
+++ test/pizza.test.js
@@ -3,3 +3,8 @@ const pizzas = require('../src/data');
 test('checks the pizza data is correct', () => {
     expect(pizzas).toMatchSnapshot();
 });
+
+test('pizzas should have a length of 4', () => {
+    expect(pizzas).toHaveLength(4);
+});
+

466393b09f829e0f0335046b3785594caf2b6990
Elbie-em <emoonga1993@gmail.com>
2020-08-24 19:10:22 +0200

    Add test for checking pizza items

--- test/pizza.test.js
+++ test/pizza.test.js
@@ -8,3 +8,11 @@ test('pizzas should have a length of 4', () => {
     expect(pizzas).toHaveLength(4);
 });
 
+test('checks for pizza names available on menu', () => {
+    expect(pizzas.map(pizza => pizza.name)).toEqual([
+        'Chicago Pizza',
+        'Neapolitan Pizza',
+        'New York Pizza',
+        'Sicilian Pizza'
+    ])
+});
